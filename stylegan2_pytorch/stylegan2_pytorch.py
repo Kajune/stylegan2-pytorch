fdb859fb78b7a644d463e3c572f3aaa19046461f
Kajune <ryuzoji21@gmail.com>
2021-01-26 23:17:38 +0900

    fix

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -341,10 +341,9 @@ class NetworkDataset(data.Dataset):
 
     def __getitem__(self, index):
         path = self.paths[index]
-        print(self.host + path)
         request = urllib.request.urlopen(self.host + path)
-        print(request)
         img = Image.open(io.BytesIO(request.read()))
+        img.save('tmp.jpg')
         return self.transform(img)
 
 # augmentations
